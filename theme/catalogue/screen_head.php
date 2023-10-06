9aa5cf8b86a144f3ae0687700e565bb049378fa5
ebabx <147089115+ebabx@users.noreply.github.com>
2023-10-06 09:25:34 +0200

    Delete theme directory

--- theme/catalogue/screen_head.php
+++
@@ -1,6 +0,0 @@
-<!-- Styles communs -->
-<link rel="stylesheet" href="<?= $theme_url ?>/css/style.css">
-
-<!-- Scripts communs  -->
-<script src="<?= $theme_url ?>/js/screen/script.js" media="screen"></script>
-
