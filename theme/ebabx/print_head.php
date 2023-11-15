3ccee9905a706b3a8a24098c3b3949aa3f51ec11
Didier Lechenne <147089115+ebabx@users.noreply.github.com>
2023-11-15 21:03:42 +0100

    add full-page plug-in

--- theme/ebabx/print_head.php
+++ theme/ebabx/print_head.php
@@ -19,6 +19,9 @@
 <script src="<?= $theme_url ?>/js/print/breakUrls.js"></script> 
 <!-- On print -->
 <script src="<?= $theme_url ?>/js/print/onPrint.js"></script>
+<!-- full-page -->
+<script src="<?= $theme_url ?>/js/print/full-page.js" type="text/javascript"></script>
+<script src="<?= $theme_url ?>/js/print/csstree.min.js" type="text/javascript"></script>
 
 <!-- Aide pour la mise en page des images -->
 <?php if(isset($_GET["layout"])): ?>
